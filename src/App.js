5958ec534f713aa22e4f8f2fe596b4733f106fb9
narenz <evershine.naren@gmail.com>
2016-11-12 12:19:56 +0000

    first draft of redux integration to App

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import React from 'react';
 import Productlist from './Components/Productlist';
 import Basket from './Components/Basket';
-import products from './Data/ProductListData.json'
+import { connect } from 'react-redux';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      productsInCart: [],
-      productsInventory: products
-    };
+  // constructor() {
+  //   super();
+  //   // this.state = {
+  //   //   productsInCart: []
+  //   // };
+  // }
+
+  componentDidMount() {
+    this.props.getProducts();
   }
 
   onAddToCart(item) {
-    const index = this.state.productsInventory.indexOf(item);
-    let modifiedItemQuantity = Object.assign(
-      {},
-      this.state.productsInventory[index],
-      { quantity: this.state.productsInventory[index].quantity - 1 })
+    //const index = this.props.productsInventory.indexOf(item);
+    // let modifiedItemQuantity = Object.assign(
+    //   {},
+    //   this.props.productsInventory[index],
+    //   { quantity: this.props.productsInventory[index].quantity - 1 })
 
-    this.setState({
-      productsInventory: [
-        ...this.state.productsInventory.slice(0, index),
-        modifiedItemQuantity,
-        ...this.state.productsInventory.slice(index + 1)
-      ],
-      productsInCart: [...this.state.productsInCart, Object.assign({}, item, { id: this.state.productsInCart.length })]
-    });
+    // this.setState({
+    //   // productsInventory: [
+    //   //   ...this.state.productsInventory.slice(0, index),
+    //   //   modifiedItemQuantity,
+    //   //   ...this.state.productsInventory.slice(index + 1)
+    //   // ],
+    //   productsInCart: [...this.state.productsInCart, Object.assign({}, item, { id: this.state.productsInCart.length })]
+    // });
   }
 
   resetBasket() {
-    this.setState({
-      productsInCart: [],
-      productsInventory: products
-    });
+    // this.setState({
+    //   productsInCart: [],
+    // });
   }
 
   checkoutBasket() {
-    this.setState({
-      productsInCart: []
-    })
+    // this.setState({
+    //   productsInCart: []
+    // })
   }
 
   render() {
@@ -48,16 +50,16 @@ class App extends React.Component {
         <h1>Shopping cart using React</h1>
         <hr />
         <Productlist
-          products={this.state.productsInventory}
+          products={this.props.productsInventory}
           onSelected={this.onAddToCart.bind(this)} />
         <hr />
         <Basket
-          prodcutsAddedtoBasket={this.state.productsInCart} />
+          prodcutsAddedtoBasket={this.props.productsInCart} />
         <button className='resetButton'
           onClick={this.resetBasket.bind(this)}
-          disabled={this.state.productsInCart.length > 0 ? '' : 'disabled'}>
+          disabled={this.props.productsInCart.length > 0 ? '' : 'disabled'}>
           Reset App
-        </button>
+          </button>
         <button className='submitButton'
           onClick={this.checkoutBasket.bind(this)}>
           Checkout
@@ -67,4 +69,20 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    productsInventory: state.productsInInventoryReducer,
+    productsInCart: state.ProductsInBasketReducer
+  };
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProducts: () => {
+      dispatch({
+        type: 'FETCH_PRODCTS'
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import products from './Data/ProductListData.json';
+
+const productsInInventoryReducer = (state = products, action) => {
+  switch (action.type) {
+    case 'RESET_APP':
+    case 'FETCH_PRODCTS':
+      state = products;
+      break;
+    default:
+      state = products;
+  }
+  return state;
+}
+
+const ProductsInBasketReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TO_BASKET':
+      break;
+    default:
+  }
+  return state;
+}
+
+const store = createStore(combineReducers({
+  productsInInventoryReducer: productsInInventoryReducer,
+  ProductsInBasketReducer: ProductsInBasketReducer
+}));
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
